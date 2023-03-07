010e624542be6942ad537bedd8573849a53d2568
Michael Charime <Michaelcharime@gmail.com>
2023-03-07 10:06:18 -0800

    Lab Work Wk4D1

---
+++ Lab-W4D2.js
@@ -0,0 +1,25 @@
+function minusOne(num) {
+    console.log(num -= 1)
+}
+
+minusOne(10)
+minusOne(100)
+minusOne(Infinity)
+
+
+
+function makeSentence(word1, word2, word3){
+    console.log(word1 + word2 + word3)
+}
+
+makeSentence("I ", "Want ", " Chimichanges")
+
+
+function getLastElement(arr){
+    console.log(arr[arr.length -1])
+}
+
+getLastElement([1, 2, 3, 4, 5, 6])
+getLastElement(['a', 'b', 'c']);
+getLastElement([[1, 2, 3], [4, 5, 6]]);
+
